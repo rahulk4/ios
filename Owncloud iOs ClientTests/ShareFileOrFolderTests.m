c5ebadeacfb5c171d096b53bfdd240c7c5e0c0c3
nalvarez <nalvarez@solidgear.es>
2015-08-21 15:13:25 +0200

    Added structure tests

--- Owncloud iOs ClientTests/ShareFileOrFolderTests.m
+++ Owncloud iOs ClientTests/ShareFileOrFolderTests.m
@@ -15,7 +15,14 @@
 @property (nonatomic) FileDto *file;
 @property (nonatomic) OCSharedDto *shareDto;
 @property (nonatomic) ShareFileOrFolder* sharedFileOrFolder;
+@property (nonatomic, strong) AppDelegate *app;
 
+
+@end
+
+@interface ShareFileOrFolder (Test)
+
+- (void)showError:(NSString *) message;
 
 @end
 
@@ -25,6 +32,7 @@
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
 
+    self.app = (AppDelegate *)[[UIApplication sharedApplication]delegate];
     self.sharedFileOrFolder = [ShareFileOrFolder new];
     
    // self.sharedFileOrFolder.delegate = self;
@@ -32,11 +40,18 @@
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
     [super tearDown];
 }
 
 - (void)testExample {
     // This is an example of a functional test case.
+    //Given-When-Then pattern
+    //given
+    
+    //when
+    
+    //then
     XCTAssert(YES, @"Pass");
     
     
@@ -44,6 +59,132 @@
 }
 
 
+
+
+
+//create fileDTO
+- (FileDto *) getFileDtoToTest {
+    
+    FileDto * file;
+    
+    file.idFile = 1;
+    file.filePath = @"";
+    file.fileName = @"fileTest";
+    file.isDirectory = 0;
+    file.userId = 1;
+    file.isDownload = 0;
+    file.size = 100;
+    //file.fileId = [rs intForColumn:@"file_id"];
+    //output.date = [rs longForColumn:@"date"];
+    //output.isFavorite = [rs intForColumn:@"is_favorite"];
+    //output.localFolder = [UtilsUrls getLocalFolderByFilePath:output.filePath andFileName:output.fileName andUserDto:user];
+    //output.etag = [rs stringForColumn:@"etag"];
+    //output.isRootFolder = [rs intForColumn:@"is_root_folder"];
+    //output.isNecessaryUpdate = [rs intForColumn:@"is_necessary_update"];
+    //output.sharedFileSource = [rs intForColumn:@"shared_file_source"];
+    //output.permissions = [rs stringForColumn:@"permissions"];
+    //output.taskIdentifier = [rs intForColumn:@"task_identifier"];
+    //output.providingFileId = [rs intForColumn:@"providing_file_id"];
+    
+    return file;
+    
+}
+
+#pragma mark ShowShareActionSheet tests
+
+- (void)testShowShareActionSheetForFileForServerShareApiSupportedAndFileIsShared {
+    
+    //given
+    APP_DELEGATE.activeUser.hasShareApiSupport = serverFunctionalitySupported;
+    FileDto *file = [self getFileDtoToTest];
+    
+    file.sharedFileSource = 1; //Already shared
+    
+    //when
+    
+    
+    //then
+    //check sheet is show
+    
+}
+
+- (void)testShowShareActionSheetForFileForServerShareApiSupportedAndFileIsNotShared {
+    
+    //given
+    APP_DELEGATE.activeUser.hasShareApiSupport = serverFunctionalitySupported;
+    FileDto *file;
+    file.sharedFileSource = 0; //No shared
+    
+    
+    //when
+    
+    
+    //then
+    //check launch the sharing
+    
+}
+
+- (void)testShowShareActionSheetForFileForServerShareApiNotCheckedAndFileIsShared {
+    
+    //given
+    APP_DELEGATE.activeUser.hasShareApiSupport = serverFunctionalityNotChecked;
+    FileDto *file;
+    
+    
+    
+    //when
+    
+    
+    //then
+    //check sheet is show
+    
+}
+
+- (void)testShowShareActionSheetForFileForServerShareApiNotCheckedAndFileIsNotShared {
+    
+    //given
+    APP_DELEGATE.activeUser.hasShareApiSupport = serverFunctionalityNotChecked;
+    FileDto *file;
+    
+    
+    
+    //when
+    
+    
+    //then
+    //check launch the sharing
+    
+}
+
+- (void)testShowShareActionSheetForFileForServerShareApiNotSupported {
+    
+    //given
+    APP_DELEGATE.activeUser.hasShareApiSupport = serverFunctionalityNotSupported;
+    FileDto *file;
+    
+    
+    
+    //when
+    
+    
+    //then
+    //check alert error is show
+    //XCTAssertTrue();
+    
+}
+
+#pragma mark Alert View Tests
+
+- (void)testShowError {
+    // This is an example of a functional test case.
+    
+    //XCTAssert([self.sharedFileOrFolder showError:@"message"], @"alert not show");
+}
+
+
+
+
+#pragma mark  Tests
 
 - (void)testClickOnShareLinkFromFileDto {
     // This is an example of a functional test case.
